ff49fbcf78be65ed681db3b909452e77d7753b1f
Pogromcamyszy <127680522+Pogromcamyszy@users.noreply.github.com>
2025-01-03 01:03:19 +0100

    Nowagalaz (#2)
    * first commit on nowa galaz
    
    * second commit on nowa galaz
    
    * third commit on nowagalaz
    
    * docker file and nginx done
    
    ---------
    
    Co-authored-by: technomancer <bozios11@gmail.com>

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the home page
 @app.route('/')
 def hello_world():
-    return 'azerbejdzan'
+    return 'Hello, World!'
 
 @app.route('/dynia')
 def dynia():
@@ -18,4 +18,4 @@ def jabko():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0', port=5000)
